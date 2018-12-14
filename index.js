75c16d6f8db6a8a340d5b0a7d9ebcb1dfa66078b
faizan <faizan1041@gmail.com>
2018-12-14 21:48:53 +0500

    Deploying for course to be published..

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+const restify = require('restify');
+const server = restify.createServer({name: "MyGymAssistant"});
+
+const PORT = process.env.PORT || 8080;
+
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.jsonp());
+
+
+
+server.post('/', (req,res,next)=>{
+    let queryResult = req.body.queryResult;
+    
+    let fulfillmentText = queryResult.fulfillmentText;
+    let outputContext = queryResult.outputContexts[0];
+
+    let exerciseType = outputContext.parameters.nextExercise;
+
+    
+
+    let exercises = ['pushups', 'pullups', 'benchpress','legpress'];
+
+    let found  = exercises.findIndex((exercise) => exercise == exerciseType);
+
+    
+
+    if(found == (exercises.length - 1)) {
+        res.json({
+            "fulfillmentText": "Okay, that's enough for today, get some rest!"
+        });
+    }
+    else {
+        outputContext.parameters.nextExercise = outputContext.parameters.exerciseType = exercises[found+1];
+        res.json({
+            "outputContexts": [outputContext],
+            "fulfillmentText": fulfillmentText.replace('_exerciseType',exercises[found+1])
+        });
+    }
+
+    return next();
+});
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
